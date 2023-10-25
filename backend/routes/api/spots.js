6ec05359e630581ad7ce8caeb929be1e03b7c052
Joel Motes <yourfavoritejoel@gmail.com>
2023-10-24 23:38:33 -0500

    get all spots owned by the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,15 +3,35 @@ const { Spot } = require('../../db/models');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const spots = await Spot.findAll();
-    res.json(spots);
-});
+router.get('/current', async (req, res) => {
+    const { user } = req;
+    let spots = {};
+    if (user) {
+        spots = await Spot.findAll({
+            include: 'User',
+            where: {
+                ownerId: user.id
+            }
+        })
+        return res.json(spots)
+    } else {
+        throw new Error('error')
+    }
+})
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     const spot = await Spot.findByPk(id);
     res.json(spot);
-})
+});
+
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll();
+    res.json(spots);
+});
+
+// router.post('/', async (req, res) => {
+//     const spot = await Spot.create(req.body)
+// })
 
 module.exports = router;
