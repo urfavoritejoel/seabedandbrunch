71e16912830e5bc62a0a95fc9da9d360f03459e9
Joel Motes <yourfavoritejoel@gmail.com>
2023-10-24 22:33:42 -0500

    get details for a spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,12 +3,15 @@ const { Spot } = require('../../db/models');
 
 const router = express.Router();
 
-
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll();
     res.json(spots);
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const spot = await Spot.findByPk(id);
+    res.json(spot);
 })
-
-
 
 module.exports = router;
