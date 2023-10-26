47964ff093cb7aba450a8d58885bac8201bfc708
Joel Motes <yourfavoritejoel@gmail.com>
2023-10-25 23:51:42 -0500

    delete review image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -3,6 +3,11 @@ const { Review, ReviewImage } = require('../../db/models');
 
 const router = express.Router();
 
-
+router.delete('/:imageId', async (req, res) => {
+    const { imageId } = req.params;
+    const image = await ReviewImage.findByPk(imageId);
+    await image.destroy();
+    res.json({ message: "Successfully deleted spot image" })
+});
 
 module.exports = router;
