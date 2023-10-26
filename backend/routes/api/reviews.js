52aba2c2abf5c713572bf779ed25e9d1940ad919
Joel Motes <yourfavoritejoel@gmail.com>
2023-10-25 22:42:29 -0500

    delete a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,10 +5,18 @@ const router = express.Router();
 
 router.put('/:reviewId', async (req, res) => {
     const { reviewId } = req.params;
+    const { user } = req
     const review = await Review.findByPk(reviewId);
     const updatedReview = await review.update(req.body);
+    updatedReview.userId = user.id
     res.json(updatedReview);
-})
+});
 
+router.delete('/:reviewId', async (req, res) => {
+    const { reviewId } = req.params;
+    const review = await Review.findByPk(reviewId);
+    await review.destroy();
+    res.json({ message: "Successfully deleted review" });
+});
 
 module.exports = router;
