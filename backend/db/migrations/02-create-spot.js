f808c62485a8c881d37fdc1565e2757043a3db3c
Joel Motes <yourfavoritejoel@gmail.com>
2023-10-30 21:12:34 -0500

    fixed create-spot down method

--- backend/db/migrations/02-create-spot.js
+++ backend/db/migrations/02-create-spot.js
@@ -67,10 +67,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Users';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['demoUser'] }
-    }, {});
+    return queryInterface.dropTable('Spots');
   }
 };
