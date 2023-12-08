92bb26f924b44c3f6332f4682d8fa49bcad6607e
Joel Motes <yourfavoritejoel@gmail.com>
2023-12-07 23:50:53 -0600

    phase 1-login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'
 
 const store = configureStore();
 // console.log(import.meta.env);
@@ -13,6 +14,7 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
