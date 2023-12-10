f6c785943bbdf817802c9c9c92f5aef33236f55b
Joel Motes <yourfavoritejoel@gmail.com>
2023-12-10 11:31:48 -0600

    sign up

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,30 @@ export const loginThunk = (user) => async (dispatch) => {
     return res;
 }
 
+export const restoreUserThunk = () => async (dispatch) => {
+    const res = await csrfFetch('/api/session');
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+}
+
+export const signupThunk = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
